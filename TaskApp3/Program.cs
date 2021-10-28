99fa616341d4b61d11445ce65f19b4dc1df779d9
QemerS <tu201806169@code.edu.az>
2021-10-28 05:06:02 +0400

    Add project files.

---
+++ TaskApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TaskApp3
+{
+    class Program
+    {
+        static int sum=0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a ededini daxil edin"); int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("b ededini daxil edin"); int b = Convert.ToInt32(Console.ReadLine());
+            sum = Hesab(a,b);
+            Console.WriteLine($"{a} ve {b} ededlerinin cemi = {sum}");
+            static int Hesab(int a, int b)
+            {
+                sum = a + b;
+                return sum;
+            }
+            
+        }
+    }
+}
